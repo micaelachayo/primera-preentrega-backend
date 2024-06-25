d3231c361cb6955b54219bd7e93907198e8342f0
Micaela Benayon <micaelabenayon@Micaelas-MacBook-Pro.local>
2024-06-25 15:27:40 -0300

    cambiando

--- src/middleware/checkProductData.middleware.js
+++ src/middleware/checkProductData.middleware.js
@@ -3,26 +3,22 @@ import productManager from "../productManager.js";
 export const checkProductData = async (req, res, next) => {
   try {
     const body = req.body;
-    const { title, description, price, thumbnail, code, stock, category } =
+    const { title, description, price, code, stock, category } =
       body;
     const newProduct = {
       title,
       description,
       price,
-      thumbnail,
       code,
       stock,
       category,
     };
     //generamos un array con todos los valores de arriba y si incluye un undifined que me tire error.
-    if (!title || !description || !price || !code || !stock || !category) {
-      return res
-        .status(400)
-        .json({ status: "error", msg: "Todos los campos son obligatorios" });
+    if (Object.values(newProduct).includes(undefined)) {
+      return res.status(400).json({ status: "error", msg: "Todos los campos son obligatorios" });
     }
     // Verificar que el código sea único
     const products = await productManager.getProducts();
-    //el some comprueba si al menos un elemento del array tiene esa condicion. es por eso q no  necesito hacer un map
     const codeExists = products.some((p) => p.code === code);
     if (codeExists) {
       return res
